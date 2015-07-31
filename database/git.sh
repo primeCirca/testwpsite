87042b3a84c8428ec59f96d9854a79dacc4214fe
Duy Do <duyphando@gmail.com>
2015-07-31 16:12:46 +0700

    Auto commit happening when the WP content (via admin GUI) is changed

--- database/git.sh
+++ database/git.sh
@@ -4,7 +4,9 @@ FilePath=$PWD/
 #implement the github flow simply
 echo Switch to branch \'content-monitor\'
 git checkout content-monitor
-echo add the file to index
+echo Add the file to index
 git add *
 echo Commit the changes in this directory \'$FilePath\':
 git commit -m "Auto commit happening when the WP content (via admin GUI) is changed"
+echo Push to the Github repo.
+git push origin content-monitor
