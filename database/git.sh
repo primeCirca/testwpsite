8b17da5b45c1f24df680939819ba0b9848da1fc7
Duy Do <duyphando@gmail.com>
2015-07-31 16:34:57 +0700

    Auto commit happening when the WP content (via admin GUI) is changed

--- database/git.sh
+++ database/git.sh
@@ -8,5 +8,7 @@ echo Add the file to index
 git add *
 echo Commit the changes in this directory \'$FilePath\':
 git commit -m "Auto commit happening when the WP content (via admin GUI) is changed"
+# cache credential forever
+git config credential.helper store
 echo Push to the Github repo.
 git push origin content-monitor
